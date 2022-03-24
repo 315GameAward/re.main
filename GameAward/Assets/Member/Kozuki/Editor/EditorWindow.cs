674d8423fa7153889f1e6559c0bdbba6cebfda6f
Daichi-Kozuki <Rouxff424@gmail.com>
2022-03-25 00:07:52 +0900

    エディタのコミット

--- GameAward/Assets/Member/Kozuki/Editor/EditorWindow.cs
+++ GameAward/Assets/Member/Kozuki/Editor/EditorWindow.cs
@@ -1,20 +1,50 @@
+//========================
+// 
+//      EditorWindow
+// 		エディタウィンドウ
+//
+//--------------------------------------------
+// 作成者：上月大地
+//========================
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System;
 
+enum IngredientUnit { Spoon, Cup, Bowl, Piece }
+
+// カスタムの Serializable クラス
+[Serializable]
+public class Ingredient
+{
+    public string name;
+    public int amount = 1;
+    private IngredientUnit unit;
+}
+
+public class Recipe : MonoBehaviour
+{
+    public Ingredient potionResult;
+    public Ingredient[] potionIngredients;
+}
 public class EditorWindowSample : EditorWindow
 {
-    // 初期の情報です
+    // 初期の情報です-------------------
     string myString = "Kozuki is Dead";
     bool groupEnabled;
     bool myBool = true;
     bool myBool2 = true;
-    float myFloat = 1.23f;
+    float myFloat = 1.0f;
+    // ---------------------------------
 
     [MenuItem("Editor/Sample")]
     private static void Create()
     {
+         //既存のウィンドウのインスタンスを表示。ない場合は作成します。
+        EditorWindow.GetWindow(typeof(EditorWindowSample));
+
         // 生成
         EditorWindowSample window = GetWindow<EditorWindowSample>("サンプル");
 
@@ -25,29 +55,34 @@ public class EditorWindowSample : EditorWindow
     /// <summary>
     /// ScriptableObjectSampleの変数
     /// </summary>
-    private ScriptableObjectSample _sample;
+    // private ScriptableObjectSample _sample;
 
     private void OnGUI()
     {
         // 実際のウィンドウのコードはここに書きます↓
-        if (_sample == null)
+        using (new GUILayout.VerticalScope())
         {
-            _sample = ScriptableObject.CreateInstance<ScriptableObjectSample>();
+            //if (_sample == null)
+            //{
+            //    _sample = ScriptableObject.CreateInstance<ScriptableObjectSample>();
+            //}
+
+            GUILayout.Label("Base Settings", EditorStyles.boldLabel);
+
+            myString = EditorGUILayout.TextField("Text Field", myString);
+
+            // これがオンになったらこれより下を弄れる
+            groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
+
+            myBool = EditorGUILayout.Toggle("Toggle", myBool);
+
+            myBool2 = EditorGUILayout.Toggle("Toggle2", myBool2);
+
+            myFloat = EditorGUILayout.Slider("Slider", myFloat, -10, 10);
+
+            // これでグループ終了
+            EditorGUILayout.EndToggleGroup();
         }
 
-        GUILayout.Label("Base Settings", EditorStyles.boldLabel);
-
-        myString = EditorGUILayout.TextField("Text Field", myString);
-
-        // これが
-        groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
-
-        myBool = EditorGUILayout.Toggle("Toggle", myBool);
-
-        myBool2 = EditorGUILayout.Toggle("Toggle2", myBool2);
-
-        myFloat = EditorGUILayout.Slider("Slider", myFloat, -10, 10);
-
-        EditorGUILayout.EndToggleGroup();
     }
 }
