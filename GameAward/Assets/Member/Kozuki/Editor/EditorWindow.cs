9f136e7653c9659f31a3761c3813b43c7f353153
Daichi-Kozuki <Rouxff424@gmail.com>
2022-03-25 22:53:49 +0900

    シーン遷移(Unity内)機能追加
    Unity内でパッとシーンを切り替える機能を追加しました。ボタンを押せば書かれたシーンへ飛びます。セーブするか聞かれたらそれまでのシーンでの変更を消されたくない場合のみ、セーブをしてください

--- GameAward/Assets/Member/Kozuki/Editor/EditorWindow.cs
+++ GameAward/Assets/Member/Kozuki/Editor/EditorWindow.cs
@@ -6,12 +6,15 @@
 //--------------------------------------------
 // 作成者：上月大地
 //========================
-
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEditor;
-using System;
+using UnityEditor.SceneManagement;
+
 
 enum IngredientUnit { Spoon, Cup, Bowl, Piece }
 
@@ -29,6 +32,7 @@ public class Recipe : MonoBehaviour
     public Ingredient potionResult;
     public Ingredient[] potionIngredients;
 }
+// サンプルウィンドウ
 public class EditorWindowSample : EditorWindow
 {
     // 初期の情報です-------------------
@@ -39,8 +43,9 @@ public class EditorWindowSample : EditorWindow
     float myFloat = 1.0f;
     // ---------------------------------
 
+    // サンプルのウィンドウ
     [MenuItem("Editor/Sample")]
-    private static void Create()
+    private static void Sample()
     {
          //既存のウィンドウのインスタンスを表示。ない場合は作成します。
         EditorWindow.GetWindow(typeof(EditorWindowSample));
@@ -51,7 +56,6 @@ public class EditorWindowSample : EditorWindow
         // 最小サイズ設定
         window.minSize = new Vector2(320, 160);
     }
-
     /// <summary>
     /// ScriptableObjectSampleの変数
     /// </summary>
@@ -84,5 +88,89 @@ public class EditorWindowSample : EditorWindow
             EditorGUILayout.EndToggleGroup();
         }
 
+    }
+}
+
+// シーンウィンドウ
+public class EditorScene : EditorWindow
+{
+    // ボタンの大きさ
+    private readonly Vector2 _buttonMinSize = new Vector2(100, 20);
+    private readonly Vector2 _buttonMaxSize = new Vector2(300, 60);
+
+    // シーンのウィンドウ
+    [MenuItem("Editor/Scene")]
+    private static void Scene()
+    {
+        //既存のウィンドウのインスタンスを表示。ない場合は作成します。
+        EditorWindow.GetWindow(typeof(EditorScene));
+
+        // 生成
+        EditorScene window = GetWindow<EditorScene>("シーン");
+
+        // 最小サイズ設定
+        window.minSize = new Vector2(320, 160);
+    }
+
+    void OnGUI()
+    {
+        // レイアウトを整える
+        GUIStyle buttonStyle = new GUIStyle("button") { fontSize = 30 };
+        var layoutOptions = new GUILayoutOption[]
+        {
+            GUILayout.MinWidth(_buttonMinSize.x),
+            GUILayout.MinHeight(_buttonMinSize.y),
+            GUILayout.MaxWidth(_buttonMaxSize.x),
+            GUILayout.MaxHeight(_buttonMaxSize.y)
+        };
+
+        // Titleボタン
+        if (GUILayout.Button("Kozk001", buttonStyle, layoutOptions))
+        {
+            // シーンを保存するか確認
+            if (!EditorSceneManager.SaveModifiedScenesIfUserWantsTo(new Scene[] { SceneManager.GetActiveScene() })) return;
+            // Titleシーンを開く
+            OpenScene("Kozk001");
+        }
+
+        // Selectボタン
+        if (GUILayout.Button("Kozk002", buttonStyle, layoutOptions))
+        {
+            // シーンを保存するか確認
+            if (!EditorSceneManager.SaveModifiedScenesIfUserWantsTo(new Scene[] { SceneManager.GetActiveScene() })) return;
+            // Titleシーンを開く
+            OpenScene("Kozk002");
+        }
+
+        // Stageボタン
+        if (GUILayout.Button("Kozk003", buttonStyle, layoutOptions))
+        {
+            // シーンを保存するか確認
+            if (!EditorSceneManager.SaveModifiedScenesIfUserWantsTo(new Scene[] { SceneManager.GetActiveScene() })) return;
+            // Titleシーンを開く
+            OpenScene("Kozk003");
+        }
+
+        // プロトタイプ
+        if (GUILayout.Button("プロトタイプ", buttonStyle, layoutOptions))
+        {
+            // シーンを保存するか確認
+            if (!EditorSceneManager.SaveModifiedScenesIfUserWantsTo(new Scene[] { SceneManager.GetActiveScene() })) return;
+            // Titleシーンを開く
+            OpenScene("prototype");
+        }
+    }
+
+    // シーンを開ける関数
+    private void OpenScene(string sceneName)
+    {
+        var sceneAssets = AssetDatabase.FindAssets("t:SceneAsset")
+            .Select(AssetDatabase.GUIDToAssetPath)
+            .Select(path => AssetDatabase.LoadAssetAtPath(path, typeof(SceneAsset)))
+            .Where(obj => obj != null)
+            .Select(obj => (SceneAsset)obj)
+            .Where(asset => asset.name == sceneName);
+        var scenePath = AssetDatabase.GetAssetPath(sceneAssets.First());
+        EditorSceneManager.OpenScene(scenePath);
     }
 }
