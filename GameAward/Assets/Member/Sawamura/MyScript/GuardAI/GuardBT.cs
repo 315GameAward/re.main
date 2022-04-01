47f6e2f697ae38b38c01745acbdcadfd7f4c3485
S_Ryuto <0141exts@gmail.com>
2022-04-01 09:48:24 +0900

    敵を検知したらその方角に向かうノード完成

--- GameAward/Assets/Member/Sawamura/MyScript/GuardAI/GuardBT.cs
+++ GameAward/Assets/Member/Sawamura/MyScript/GuardAI/GuardBT.cs
@@ -1,14 +1,25 @@
-﻿using BehaviorTree;
+﻿using System.Collections.Generic;
+using BehaviorTree;
 
 public class GuardBT : Tree
 {
     public UnityEngine.Transform[] waypoints;
 
     public static float speed = 2f;
+    public static float fovRange = 6f;
 
     protected override Node SetupTree()
     {
-        Node root = new TaskPatrol(transform, waypoints);
+        Node root = new Selector(new List<Node>
+        {
+            new Selector(new List<Node>
+            {
+                new CheckEnemyInFOVRange(transform),
+                new TaskGoToTarget(transform),
+            }),
+            new TaskPatrol(transform,waypoints),
+        });
+
 
         return root;
     } 
