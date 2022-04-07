9c090aeae384d32489af1c4594892042119705ce
Daichi-Kozuki <Rouxff424@gmail.com>
2022-04-07 18:04:40 +0900

    コウヅキのシーンを実行すると体力が表示されるようにしました

--- GameAward/Assets/Member/Kozuki/Kozk_Script/Life.cs
+++ GameAward/Assets/Member/Kozuki/Kozk_Script/Life.cs
@@ -13,31 +13,52 @@ using UnityEngine;
 public class Life : MonoBehaviour
 {
     // 体力
-    public int pLife;
+    public int nLife;
+
+    [SerializeField]
+    GameObject prefab;      // インスペクターからハートのプレハブを割り当てる
+
+    List<GameObject> hearts = new List<GameObject>();   // 生成したハートを入れる
+   
+    //========================
+    //
+    // ハートを1つ減らす関数
+    //
+    //========================
+    public void DelLife()
+    {
+        nLife--;
+        Destroy(hearts[0]);
+        hearts.RemoveAt(0);
+    }
+
+    //========================
+    //
+    // ハートを1つ増やす関数
+    //
+    //========================
+    public void Increase()
+    {
+        // 引数2はどのオブジェクトの子にするかで、引数3は子にする際に以前の位置を保つか(LayoutGroup系ではfalseにしないとおかしくなる)
+        GameObject instance = Instantiate(prefab, transform, false);
+        hearts.Add(instance);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-   
+        for (int i = nLife; i > 0; i--)
+        {
+            Increase();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(pLife <= 0)
+        if(nLife <= 0)
         {
             // ゲームオーバー呼び出し
         }
-    }
-
-    //========================
-    //      
-    //      体力減少関数
-    //
-    //========================
-    public void DelLife()
-    {
-        // 体力を減らす
-        pLife--;
     }
 }
