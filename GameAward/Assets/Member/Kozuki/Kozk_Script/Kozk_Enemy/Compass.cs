1ca93773e60572a63b82b9031f037c5ad4806e71
Daichi-Kozuki <Rouxff424@gmail.com>
2022-04-15 22:42:37 +0900

    僕のフォルダにスクリプト追加、プレハブ追加

---
+++ GameAward/Assets/Member/Kozuki/Kozk_Script/Kozk_Enemy/Compass.cs
@@ -0,0 +1,26 @@
+//========================
+// 
+//      Compass.cs
+// 		コンパスエネミー
+//
+//--------------------------------------------
+// 作成者：上月大地
+//========================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 始まったら
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ゲーム中
+    }
+}
