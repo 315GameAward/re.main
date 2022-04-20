7e9643fdfcd9e7f95a3d1c9de31c28e0caa26aa2
Daichi-Kozuki <Rouxff424@gmail.com>
2022-04-20 11:57:55 +0900

    コンパス強化

--- GameAward/Assets/Member/Kozuki/Kozk_Script/Life.cs
+++ GameAward/Assets/Member/Kozuki/Kozk_Script/Life.cs
@@ -33,7 +33,7 @@ public class Life : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(nLife);
+       // Debug.Log(nLife);
 
         if (nLife <= 0)
         {
