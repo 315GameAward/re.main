d8a715e6ee84d4e3013a34984bfb49a46c798cb0
Daichi-Kozuki <Rouxff424@gmail.com>
2022-04-27 09:56:06 +0900

    コミット

--- GameAward/Assets/Member/Kozuki/Kozk_Script/Kozk_Enemy/SetPosition.cs
+++ GameAward/Assets/Member/Kozuki/Kozk_Script/Kozk_Enemy/SetPosition.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class SetPosition : MonoBehaviour
 {
+    private Rigidbody rb;
+    private int upForce;
+    private float distance;
+
     //初期位置
     private Vector3 startPosition;
     //目的地
@@ -13,6 +17,24 @@ public class SetPosition : MonoBehaviour
         //　初期位置を設定
         startPosition = transform.position;
         SetDestination(transform.position);
+
+        rb = GetComponent<Rigidbody>();
+        upForce = 300;
+        distance = 1.0f;
+    }
+    private void Update()
+    {
+        Vector3 rayPosition = transform.position + new Vector3(0.0f, 0.0f, 0.0f);
+        Ray ray = new Ray(rayPosition, Vector3.down);
+        bool isGround = Physics.Raycast(ray, distance);
+        Debug.DrawRay(rayPosition, Vector3.down * distance, Color.red);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.AddForce(new Vector3(0, upForce, 0));
+        }
+
+        Debug.Log(isGround);
     }
 
     //　ランダムな位置の作成
