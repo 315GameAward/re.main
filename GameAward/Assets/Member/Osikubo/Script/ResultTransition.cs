fd2adfa8dfb892e49b404a80b8adb179ffc3f256
hina <95481074+OshikuboHina@users.noreply.github.com>
2022-05-10 20:10:28 +0900

    リザルトシーンに飛べるようにしました

---
+++ GameAward/Assets/Member/Osikubo/Script/ResultTransition.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResultTransition : MonoBehaviour
+{
+    private GameObject[] ResultTest;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        ResultTest = GameObject.FindGameObjectsWithTag("kesigomu");
+        Debug.Log("Á‚µƒSƒ€‚Ì”" + ResultTest.Length);
+        if (ResultTest.Length == 0)
+        {
+            Debug.Log("ƒV[ƒ“‚ÌˆÚ“®");
+            SceneManager.LoadScene("ResultScene");
+        }
+    }
+}
