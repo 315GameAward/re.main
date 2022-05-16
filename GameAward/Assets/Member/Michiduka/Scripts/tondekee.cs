b9e57abb6b24a74e26ad15abae7fe391147ea7f7
mitch <54772425+michiduka@users.noreply.github.com>
2022-05-16 17:21:47 +0900

    tondekee.cs 修正

--- GameAward/Assets/Member/Michiduka/Scripts/tondekee.cs
+++ GameAward/Assets/Member/Michiduka/Scripts/tondekee.cs
@@ -15,6 +15,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class tondekee : MonoBehaviour
 {
@@ -25,12 +26,21 @@ public class tondekee : MonoBehaviour
     private Vector3 spawnPos;
     private Vector3 pos;    //座標
 
+    public Life life;
+
     // Start is called before the first frame update
     void Start()
     {
+
+    }
+
+    void Update()
+    {
         //テスト用
-        //StartCoroutine("Tondekee");
-        SpawnParticle();
+        if (Keyboard.current.rKey.wasPressedThisFrame)
+        {
+            SpawnParticle();
+        }
     }
 
     void SpawnParticle()
@@ -38,23 +48,23 @@ public class tondekee : MonoBehaviour
         //パーティクル設置
         //targetParticleInstance = GameObject.Instantiate(targetParticle, spawnPos, transform.rotation) as GameObject;
         targetParticleInstance = GameObject.Instantiate(targetParticle, new Vector3(0, 0, 0), transform.rotation) as GameObject;
+        //z 1.5
+        targetParticleInstance.transform.Translate(0, 0, 1.95f);
         StartCoroutine("Tondekee");
+        //ライフ消去
+        life.DelLife();
     }
 
     IEnumerator Tondekee()
     {
-        while (pos.y < 6.35f)
+        //while (pos.y < 6.35f && pos.x < 2.1f)
+        while (pos.x < 2.1f)
         {
             pos = targetParticleInstance.transform.position;
-            targetParticleInstance.transform.Translate(0, 0.02f, 0);
-            yield return new WaitForSeconds(0.01f);
-        }
-
-        while (pos.x < 3.1f)
-        {
-            pos = targetParticleInstance.transform.position;
+            targetParticleInstance.transform.Translate(0, 0.05f, 0);
             targetParticleInstance.transform.Translate(0.02f, 0, 0);
             yield return new WaitForSeconds(0.01f);
         }
+        
     }
 }
