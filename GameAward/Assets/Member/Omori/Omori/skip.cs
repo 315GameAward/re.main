2594bd4d745b12ca48449c0d593e9d265402b559
shichimi07 <95484225+shichimi07@users.noreply.github.com>
2022-05-18 12:10:44 +0900

    skip

---
+++ GameAward/Assets/Member/Omori/Omori/skip.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Playables;
+
+public class skip : MonoBehaviour
+{
+    TimelinePreferences timeline;
+    private PlayableDirector director;
+    public GameObject controlPanel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       // timeline = GetComponent<TimelinePreferences>();
+        director = GetComponent<PlayableDirector>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // director.time = 10;
+        
+        // スペースキーを押したら
+        if(Keyboard.current.spaceKey.isPressed)
+        {
+            director.initialTime = 10;
+            // Time.e = 30;
+        }
+    }
+}
