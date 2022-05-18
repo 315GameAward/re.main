3466e171aef42b0c92554885d1f2f349456de1a3
mitch <54772425+michiduka@users.noreply.github.com>
2022-05-18 12:00:15 +0900

    Add files via upload

--- GameAward/Assets/Member/Michiduka/Scripts/tondekee.cs
+++ GameAward/Assets/Member/Michiduka/Scripts/tondekee.cs
@@ -24,10 +24,11 @@ public class tondekee : MonoBehaviour
     private GameObject targetParticleInstance;
 
     private Vector3 spawnPos;   //スポーン座標
-    private Vector3 pos;        //座標
 
     //対象ライフ
     [SerializeField] private GameObject targetLife;
+    //対象ライフA
+    [SerializeField] private GameObject targetLifeA;
 
     public Animator targetAnimator;
 
@@ -51,33 +52,53 @@ public class tondekee : MonoBehaviour
         //パーティクル設置
         //targetParticleInstance = GameObject.Instantiate(targetParticle, spawnPos, transform.rotation) as GameObject;
         targetParticleInstance = GameObject.Instantiate(targetParticle, new Vector3(0, 0, 0), transform.rotation) as GameObject;
-        //z 1.5
-        targetParticleInstance.transform.Translate(0, 0, 1.45f);
+        targetParticleInstance.transform.Translate(0, 0, 1f);
         StartCoroutine("Tondekee");
     }
 
     IEnumerator Tondekee()
     {
         //while (pos.y < 6.35f && pos.x < 2.1f)
-        while (pos.x < 2.1f)
+
+        while (targetParticleInstance.transform.position.y < 4.55f)
         {
-            pos = targetParticleInstance.transform.position;
-            targetParticleInstance.transform.Translate(0, 0.061f, 0);
-            targetParticleInstance.transform.Translate(0.02f, 0, 0);
+            targetParticleInstance.transform.Translate(0, 0.05f, 0);
+            if (Life.instance.nLife == 3)
+                targetParticleInstance.transform.Translate(0.035f, 0, 0);
+            else if (Life.instance.nLife == 2)
+                targetParticleInstance.transform.Translate(0.029f, 0, 0);
+            else if (Life.instance.nLife == 1)
+                targetParticleInstance.transform.Translate(0.024f, 0, 0);
             yield return new WaitForSeconds(0.01f);
         }
-
-        //ライフ設置
-        targetLife.SetActive(true);
-        Debug.Log("おわり");
 
         //ライフ消去
         Life.instance.DelLife();
 
+        //ライフ設置
+        targetLife.SetActive(true);
+        Debug.Log(Life.instance.nLife);
+
+        //1 = 335.3
+        //2 = 410.4
+        //3 = 485.2
+
+        if (Life.instance.nLife == 1)
+        {
+            targetLifeA.transform.Translate(-75.1f, 0.0f, 0.0f);
+        }
+        else if (Life.instance.nLife == 0)
+        {
+            targetLifeA.transform.Translate(-75.1f, 0.0f, 0.0f);
+        }
+
         targetAnimator.Play("Life");
         yield return new WaitForSeconds(.5f);
 
+        //ライフ隠
         targetLife.SetActive(false);
 
+        //破壊
+        Destroy(targetParticleInstance);
     }
 }
