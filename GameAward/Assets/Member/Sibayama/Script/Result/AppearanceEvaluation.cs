8ed9e67d17ccfcef4b360e0336af840ae43a6d7f
Rintaro-B <kemukemubasara@gmail.com>
2022-05-20 22:07:32 +0900

    リザルト更新
    アニメーターコンポーネント追加。
    リザルトの評価項目を表示させるアニメーション追加。
    その処理を行うためのスクリプトのガワ追加。評価用テクスチャを入れる変数をコンポーネントに表示。

---
+++ GameAward/Assets/Member/Sibayama/Script/Result/AppearanceEvaluation.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearanceEvaluation : MonoBehaviour
+{
+    // 評価に使うスタンプテクスチャ
+    public Sprite[] evaluation = new Sprite[3];
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
