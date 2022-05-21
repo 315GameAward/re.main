bc8d843f9602df4e7e9d6d69dc4e733b2453ded5
Rintaro-B <kemukemubasara@gmail.com>
2022-05-21 19:30:05 +0900

    評価(ガワ)の作成

--- GameAward/Assets/Member/Sibayama/Script/Result/AppearanceEvaluation.cs
+++ GameAward/Assets/Member/Sibayama/Script/Result/AppearanceEvaluation.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class AppearanceEvaluation : MonoBehaviour
 {
+    // 評価
+    public enum EvaType
+    {
+        Too_Bad,
+        Good,
+        Great,
+
+        Max_Type
+    }
+
     // 評価に使うスタンプテクスチャ
-    public Sprite[] evaluation = new Sprite[3];
+    public Sprite[] evaluation = new Sprite[(int)EvaType.Max_Type];
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +26,10 @@ public class AppearanceEvaluation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        switch ()
+        {
+            default:
+                break;
+        }
     }
 }
