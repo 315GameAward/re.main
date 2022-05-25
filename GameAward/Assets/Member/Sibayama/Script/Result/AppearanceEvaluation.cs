7ae37a77990904373f81bf7671bf801765fabd82
Rintaro-B <kemukemubasara@gmail.com>
2022-05-25 12:07:05 +0900

    UpdateEvaluation
    一部privateで処理するよう変更しました

--- GameAward/Assets/Member/Sibayama/Script/Result/AppearanceEvaluation.cs
+++ GameAward/Assets/Member/Sibayama/Script/Result/AppearanceEvaluation.cs
@@ -27,33 +27,44 @@ public class AppearanceEvaluation : MonoBehaviour
         Max_Type
     }
 
-    public GameObject SelectButton;
     public const int Max_Obj = 2;  // 評価項目数
     
     public float stopTime;  // スタンプ押す間隔
 
-    static bool[] activeObj = new bool[2];  // オブジェクトをアクティブに設定
+    //static bool[] activeObj = new bool[2];  // オブジェクトをアクティブに設定
     
     // 評価テクスチャを適用させるオブジェクト
     public GameObject[] item = new GameObject[Max_Obj];
 
-    // どデカスタンプ
-    GameObject last_result;
+    
+    GameObject layer;   // どデカスタンプ押した後のリザルトぼかし
+
+    // アクティブ状態を管理するオブジェクト
+    GameObject[] AcObj = new GameObject[Max_Obj];
     
     // 評価に使うスタンプテクスチャ
     public Sprite[] evaluation = new Sprite[(int)EvaType.Max_Type];
 
     private void Awake()
     {
-        SelectButton.SetActive(false);
+        layer = GameObject.Find("layer");
+        layer.gameObject.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+        AcObj[0] = GameObject.Find("layer/last_result");
+        AcObj[0].GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+        AcObj[1] = GameObject.Find("layer/SelectButton");
+
+        for (var i = 0; i < Max_Obj; ++i)
+        {
+            AcObj[i].SetActive(false);
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        last_result = GameObject.Find("last_result");
-        last_result.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
-        GameObject.Find("layer").gameObject.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+        
+        
+        
         // 評価項目を採点
         StampEvaluation();
     }
@@ -71,27 +82,26 @@ public class AppearanceEvaluation : MonoBehaviour
     // 仮の評価処理
     void StampEvaluation()
     {
-        for (int i = 0; i < Max_Obj; ++i)
+        item[0].GetComponent<Image>().sprite = evaluation[2];
+        item[0].GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+        switch (1)
         {
-            switch (i)
-            {
-                case 0:
-                    item[i].GetComponent<Image>().sprite = evaluation[0];
-                    item[i].GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
-                    break;
-                case 1:
-                    item[i].GetComponent<Image>().sprite = evaluation[1];
-                    item[i].GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+            case 0:
+                item[1].GetComponent<Image>().sprite = evaluation[0];
+                item[1].GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+                break;
+            case 1:
+                item[1].GetComponent<Image>().sprite = evaluation[1];
+                item[1].GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
 
-                    break;
-                case 2:
-                    item[i].GetComponent<Image>().sprite = evaluation[2];
-                    item[i].GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+                break;
+            case 2:
+                item[1].GetComponent<Image>().sprite = evaluation[2];
+                item[1].GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
 
-                    break;
-                default:
-                    break;
-            }
+                break;
+            default:
+                break;
         }
     }
 
@@ -105,13 +115,12 @@ public class AppearanceEvaluation : MonoBehaviour
             item[i].GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
         }
         yield return new WaitForSeconds(stopTime);
-        last_result.GetComponent<Image>().color = new Color(0.0f, 0.0f, 1.0f, 1.0f);
-        //activeObj[0] = true;
-        yield return new WaitForSeconds(stopTime);
-        GameObject.Find("layer").gameObject.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.5f);
-        SelectButton.SetActive(true);
-        //activeObj[1] = true;
-        //SetActive.AcObj.SetActive(true);
-    }
 
+        AcObj[0].SetActive(true);
+        AcObj[0].GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        yield return new WaitForSeconds(stopTime);
+
+        AcObj[1].SetActive(true);
+        layer.gameObject.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.5f);   
+    }
 }
