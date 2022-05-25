64c2a27cf5faaf88083cc38f087cdbc3d3a826d5
S_Ryuto <0141exts@gmail.com>
2022-05-25 10:13:21 +0900

    スクリプト編集

--- GameAward/Assets/Scenes/Alpha/GameClear_Over/IventGet_Dest_a.cs
+++ GameAward/Assets/Scenes/Alpha/GameClear_Over/IventGet_Dest_a.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public enum DestGame_a
-{
-    GameStart,      //ゲームスタート
-    GameClear,
-    GameOver        //ゲームオーバー
-}
+//public enum DestGame_a
+//{
+//    GameStart,      //ゲームスタート
+//    GameClear,
+//    GameOver        //ゲームオーバー
+//}
 public class IventGet_Dest_a : MonoBehaviour
 {
     //クリア用UI
-    [SerializeField]
-    GameObject clearUI;
+    //[SerializeField]
+    //GameObject clearUI;
 
-    public DestGame_a status_dest;
+    //public DestGame_a status_dest;
     public IventPush_Dest_a[] target;
 
     int i = 0;
@@ -30,26 +30,26 @@ public class IventGet_Dest_a : MonoBehaviour
         target[i].OnDestroyed.AddListener(() => {
             Debug.Log("targetが削除されました");
             // ここに処理を追加
-            status_dest = DestGame_a.GameClear;
+            //status_dest = DestGame_a.GameClear;
         });
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        status_dest = DestGame_a.GameStart;
+        //status_dest = DestGame_a.GameStart;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (status_dest == DestGame_a.GameClear　|| Input.GetKeyUp(KeyCode.V))
-        {
-            clearUI.SetActive(true);
-        }
-        if (Input.GetKeyUp(KeyCode.R))
-        {
-            clearUI.SetActive(false);
-        }
-    }
+    //void Update()
+    //{
+    //    if (status_dest == DestGame_a.GameClear　|| Input.GetKeyUp(KeyCode.V))
+    //    {
+    //        clearUI.SetActive(true);
+    //    }
+    //    if (Input.GetKeyUp(KeyCode.R))
+    //    {
+    //        clearUI.SetActive(false);
+    //    }
+    //}
 }
