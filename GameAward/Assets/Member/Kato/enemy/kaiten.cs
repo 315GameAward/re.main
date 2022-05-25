93be40f9df1f41689a2b36a601e634dc95bb2aa3
加藤慧悟 <porigon2@docomo.ne.jp>
2022-05-25 10:04:50 +0900

    コミット

--- GameAward/Assets/Member/Kato/enemy/kaiten.cs
+++ GameAward/Assets/Member/Kato/enemy/kaiten.cs
@@ -6,12 +6,44 @@ public class kaiten : MonoBehaviour
 {
     // Start is called before the first frame update
     float z;
+    bool hitground;
+    Enemy enemy;
+    public bool Hitground
+    {
+        get
+        {
+            return hitground;
+        }
+    }
+    void Start()
+    {
+        Enemy enemy;
+        enemy = gameObject.GetComponent<Enemy>();
+    }
     void Update()
     {
-        z += Time.deltaTime * 1;
-        transform.Rotate(0, z, 0);
-        
-        //transform.rotation = Quaternion.Euler(y, 0, 0);
-        //gameObject.transform.localRotation = Quaternion.Euler(-90, 0, z);
+        ray();
+
+        if (Hitground == false)
+        {
+            transform.Rotate(0, 1, 0);
+        }
+        if (Hitground == true)
+        {
+            Debug.Log("debug comment");
+            transform.Rotate(0, 0, 0);
+        }
+
+    }
+    void ray()
+    {
+        int back = LayerMask.GetMask("BackGround");
+        Vector3 foll;
+
+        RaycastHit rayhit;
+        //Raybrockhit 
+        rayhit = new RaycastHit();
+        foll = new Vector3(0, -1, 0);
+        hitground = Physics.Raycast(transform.position, foll, out rayhit, 1, back, QueryTriggerInteraction.Ignore);
     }
 }
