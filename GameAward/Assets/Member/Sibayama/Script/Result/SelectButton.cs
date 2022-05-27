fd779c4ebc2d6718408945461d07f8bfb137f48e
Rintaro-B <kemukemubasara@gmail.com>
2022-05-28 07:43:10 +0900

    リザルト更新
    戻るボタンに仮でエリアセレクトに遷移するようにしました。

--- GameAward/Assets/Member/Sibayama/Script/Result/SelectButton.cs
+++ GameAward/Assets/Member/Sibayama/Script/Result/SelectButton.cs
@@ -13,14 +13,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SelectButton : MonoBehaviour
 {
-    //Button[] button = new Button[2];
+    Button[] button = new Button[2];
     // Start is called before the first frame update
     void Start()
     {
-        //button[0] = GameObject.Find("ReturnSelect").GetComponent<Button>();
+        button[0] = GameObject.Find("ReturnSelect").GetComponent<Button>();
+        button[1] = GameObject.Find("NextStage").GetComponent<Button>();
+        button[0].Select();
     }
 
     // Update is called once per frame
@@ -28,4 +31,15 @@ public class SelectButton : MonoBehaviour
     {
         
     }
+
+    public void ReturnSelect(string _name)
+    {
+        SceneManager.LoadScene(_name);
+    }
+
+    public void NextStage(int _index)
+    {
+        SceneManager.LoadScene(_index);
+    }
+
 }
