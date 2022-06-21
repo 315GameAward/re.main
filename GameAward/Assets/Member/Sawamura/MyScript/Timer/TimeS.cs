8494032571297780f21fe93ec4b6354e9b128d7f
S_Ryuto <0141exts@gmail.com>
2022-06-21 14:13:38 +0900

    timeの針用のスクリプト完成

---
+++ GameAward/Assets/Member/Sawamura/MyScript/Timer/TimeS.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// /ƒNƒ‰ƒX–¼‚ðTime‚É‚·‚é‚ÆTime‚Ì’è‹`‚ª‚ ‚â‚Ó‚â‚É‚È‚é
+/// </summary>
+public class TimeS : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, 360 / 60* Time.deltaTime);
+    }
+}
